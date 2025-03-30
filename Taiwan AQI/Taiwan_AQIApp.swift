0a575ad1bf4f0c99b7f1e90cdfd065ea771acc0e
Oh-Joe <antoimn@gmail.com>
2025-03-30 12:18:35 +0800

    Initial Commit

---
+++ Taiwan AQI/Taiwan_AQIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taiwan_AQIApp.swift
+//  Taiwan AQI
+//
+//  Created by Antoine Moreau on 2025/3/30.
+//
+
+import SwiftUI
+
+@main
+struct Taiwan_AQIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
